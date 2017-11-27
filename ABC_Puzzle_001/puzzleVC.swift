14dd83b3b16f42877e94419f71455c85cefd7723
Joshisaka1004 <joshi1004@gmail.com>
2017-11-27 11:06:48 +0100

    Trying to give each button a tag programmatically, seems to work fine (I know it is easier to do this in the IB)

---
+++ ABC_Puzzle_001/puzzleVC.swift
@@ -0,0 +1,37 @@
+//
+//  puzzleVC.swift
+//  ABC_Puzzle_001
+//
+//  Created by Joachim Vetter on 27.11.17.
+//  Copyright © 2017 Joachim Vetter. All rights reserved.
+//
+
+import UIKit
+
+class puzzleVC: UIViewController {
+
+    @IBOutlet weak var verticalStack: UIStackView!
+    var stack1 = [Int]()
+    var increase = 0
+    @IBOutlet var horizontalStacks: [UIStackView]!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        giveTagsToButtons()
+    }
+
+    func giveTagsToButtons() {
+        for k in 0...5 {
+            stack1.append(k)
+        }
+        for i in horizontalStacks {
+            for j in 0..<(i.subviews as! [UIButton]).count {
+                (i.subviews[j] as! UIButton).tag = stack1[j] + increase
+            }
+            increase += 6
+        }
+    }
+    
+    @IBAction func myB1(_ sender: UIButton) {
+        print(sender.tag)
+    }
+}
